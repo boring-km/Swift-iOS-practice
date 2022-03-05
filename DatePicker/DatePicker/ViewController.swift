f1e68162d6f8b99e9e77615466338d44665867b8
boring-km <kms0644804@naver.com>
2022-03-05 22:45:50 +0900

    iOS, Swift, Storyboard 공부 시작

---
+++ DatePicker/DatePicker/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  DatePicker
+//
+//  Created by kangmin on 2022/03/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    let timeSelector: Selector = #selector(ViewController.updateTime)
+    let interval = 1.0
+    var count = 0
+    
+    @IBOutlet weak var lblCurrentTime: UILabel!
+    @IBOutlet weak var lblPickerTime: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        Timer.scheduledTimer(timeInterval: interval, target: self, selector: timeSelector, userInfo: nil, repeats: true)
+    }
+
+    @IBAction func changeDatePicker(_ sender: UIDatePicker) {
+        let datePickerView = sender
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm EEE"
+        lblPickerTime.text = "선택시간: " + formatter.string(from: datePickerView.date)
+    }
+    
+    @objc func updateTime() {
+        let date = NSDate()
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss EEE"
+        lblCurrentTime.text = "현재시간: " + formatter.string(from: date as Date)
+    }
+    
+}
+
---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by kangmin on 2022/03/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
