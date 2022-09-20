45d6019a7060f0783d9da9187b2f3f98841f8d9d
boring-km <kms0644804@naver.com>
2022-09-21 00:29:06 +0900

    SwiftUI AsyncImage 따라해보기

---
+++ SwiftUIAsyncImage/SwiftUIAsyncImage/SwiftUIAsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAsyncImageApp.swift
+//  SwiftUIAsyncImage
+//
+//  Created by kangmin on 2022/09/19.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
