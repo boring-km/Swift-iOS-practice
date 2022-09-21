5bac972d5b5d7db79a60cba2823d7df802474477
boring-km <kms0644804@naver.com>
2022-09-21 16:56:42 +0900

    Restart App Add Colors

---
+++ Restart-App/Students/Restart/Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Kangmin Jin on 2022/09/21.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
